5840c8611c1c9ebc522ba4e892d297ce5bf31bfb
David Thole <dthole@gmail.com>
2019-01-21 14:26:38 -0600

    Adding .net to the interactive notes, adding a few more dependencies to the setup.sh

--- interactive.sh
+++ interactive.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+### Simple Commands (ways of executing something)
+
 # This doesn't work, container has no understanding of src/genseq
 docker run -it ubuntu sh src/genseq.sh
 
@@ -10,10 +12,21 @@ docker run -v$PWD:/in -it ubuntu sh /in/src/genseq.sh
 cat src/genseq.sh|docker run -a stdin -a stdout -i ubuntu /bin/sh
 
 
-# But how to write out....
+### But how to write out....
 
 # From Host Viewpoint
 docker run -v$PWD:/in -it ubuntu sh /in/src/genseq.sh > temp/genseq.txt
 
 # From Client Viewpoint
 docker run -v$PWD:/in -it ubuntu sh -c "sh /in/src/genseq.sh > in/temp/genseq.txt"
+
+### Developing Software ...
+
+# Execute actual code (hard written code)
+docker run --rm -v $PWD:/app microsoft/dotnet dotnet run --project app/dotnet
+
+# Interactive Shell
+docker run --rm -v $PWD:/app -it microsoft/dotnet /bin/bash
+# Then run cd app/dotnet
+# Then run dotnet run
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Because we ignore this directory, yet scripts need it.
+mkdir temp
+
 docker pull ubuntu
 docker pull microsoft/dotnet
+docker pull thedarktrumpet/rstudiojava
+docker pull microsoft/mssql-server-linux
