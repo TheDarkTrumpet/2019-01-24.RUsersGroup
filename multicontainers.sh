d9e5cfcb8f51582aaf9d893a4aa0f307a7b66cfd
David Thole <dthole@gmail.com>
2019-01-23 20:22:59 -0600

    optimizing the multicontainers a bit

--- multicontainers.sh
+++ multicontainers.sh
@@ -8,5 +8,5 @@ docker run -v$PWD:/in -it ubuntu sh /in/src/genseq.sh
 docker run -v$PWD:/in -t ubuntu sh -c "stty -onlcr && /in/src/genseq.sh" | docker run -v$PWD:/in -i ubuntu /in/src/pick5th.sh
 
 # Method 2 - Using a temporary storage location
-docker run -v$PWD:/in -it ubuntu sh -c "stty -onlcr && /in/src/genseq.sh" > temp/sequence.txt
-docker run -v$PWD:/in -it ubuntu sh -c "cat /in/temp/sequence.txt | /in/src/pick5th.sh"
+docker run -v$PWD:/in -t ubuntu sh -c "stty -onlcr && /in/src/genseq.sh" > temp/sequence.txt
+docker run -v$PWD:/in -t ubuntu sh -c "cat /in/temp/sequence.txt | /in/src/pick5th.sh"
