8c7aca368be8311822b6b1cf7d116ebadccb95ba
David Thole <dthole@gmail.com>
2019-01-24 14:11:27 -0600

    Some style changes to make it more clear - from the meeting

--- interactive.sh
+++ interactive.sh
@@ -9,7 +9,7 @@ docker run -it ubuntu sh src/genseq.sh
 docker run -v$PWD:/in -it ubuntu sh /in/src/genseq.sh
 
 # ..but can also be read from std in, and write to std out:
-cat src/genseq.sh|docker run -a stdin -a stdout -i ubuntu /bin/sh
+cat src/genseq.sh | docker run -a stdin -a stdout -i ubuntu /bin/sh
 
 
 ### But how to write out....
@@ -17,16 +17,16 @@ cat src/genseq.sh|docker run -a stdin -a stdout -i ubuntu /bin/sh
 # From Host Viewpoint
 docker run -v$PWD:/in -it ubuntu sh /in/src/genseq.sh > temp/genseq.txt
 
-# From Client Viewpoint
+# From Container Viewpoint
 docker run -v$PWD:/in -it ubuntu sh -c "sh /in/src/genseq.sh > in/temp/genseq.txt"
 
 ### Developing Software ...
 
 # Execute actual code (hard written code)
-docker run --rm -v $PWD:/app microsoft/dotnet dotnet run --project app/dotnet
+docker run --rm -v$PWD:/app microsoft/dotnet dotnet run --project app/dotnet
 
 # Interactive Shell
-docker run --rm -v $PWD:/app -it microsoft/dotnet /bin/bash
+docker run --rm -v$PWD:/app -it microsoft/dotnet /bin/bash
 # Then run cd app/dotnet
 # Then run dotnet run
 
